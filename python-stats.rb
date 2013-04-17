7986f94a19b9df03637037ff5889309498797863
Lars Yencken <lars@yencken.org>
2013-04-17 14:49:12 +1000

    Normalised python for statistics dep.

--- python-stats.rb
+++ python-stats.rb
@@ -1,121 +1,98 @@
 dep 'python for statistics' do
-    requires {
-        on :osx, [
-            'python',
-            'numpy',
-            'scipy',
-            'ipython',
-            'matplotlib',
-            'pandas'
-        ]
-        on :ubuntu, [
-            'python',
-            'python-dev.managed',
-            'numpy',
-            'scipy',
-            'ipython',
-            'matplotlib',
-            'pandas'
-        ]
-    }
+  requires 'python',
+    'numpy',
+    'scipy',
+    'ipython',
+    'matplotlib',
+    'pandas',
+    'networkx'
 end
 
-dep 'python', :template => 'managed' do
-    installs {
-        via :apt, 'python'
-        via :brew, 'python'
-    }
-    provides ['python']
-end
-
-dep 'python-dev.managed' do
-    provides []
-end
-
-dep 'numpy.pip' do
-    provides []
-end
-
-dep 'scipy' do
-    requires {
-        on :osx, 'python', 'scipy.pip'
-        on :ubuntu, 'python', 'scipy.managed'
-    }
-end
-
-dep 'scipy.managed' do
-    installs {
-        on :ubuntu, 'python-scipy'
-    }
-    provides []
-end
-
-dep 'scipy.pip' do
-    provides []
-end
+#---------------------------------------------------------------------------#
 
 dep 'ipython', :template => 'pip' do
-end
-
-dep 'matplotlib' do
-    requires {
-        on :osx, 'python', 'matplotlib.pip'
-        on :ubuntu, 'python', 'matplotlib.managed'
-    }
-end
-
-dep 'matplotlib.managed' do
-    installs {
-        on :ubuntu, 'python-matplotlib'
-    }
-    provides []
-end
-
-dep 'matplotlib.pip' do
-    provides []
-end
-
-dep 'pandas', :template => 'pip' do
-    provides []
-end
-
-dep 'numpy' do
-    requires {
-        on :osx, 'numpy.pip'
-        on :ubuntu, 'numpy.managed'
-    }
-end
-
-dep 'numpy.managed' do
-    installs {
-        via :apt, 'python-numpy'
-    }
-    provides []
-end
-
-dep 'scipy.managed' do
-    installs {
-        via :apt, 'python-scipy'
-    }
-    provides []
+  provides ['ipython']
 end
 
 dep 'ipython.managed' do
-    installs {
-        via :apt, 'ipython'
-    }
-    provides ['ipython']
+  installs 'ipython'
+  provides ['ipython']
+end
+
+dep 'matplotlib' do
+  requires {
+    on :osx, 'python', 'matplotlib.pip'
+    on :ubuntu, 'python', 'matplotlib.managed'
+  }
 end
 
 dep 'matplotlib.managed' do
-    installs {
-        via :apt, 'python-matplotlib'
-    }
+  installs 'python-matplotlib'
+  provides []
+end
+
+dep 'matplotlib.pip' do
+  provides []
+end
+
+dep 'networkx', :template => 'pip' do
+  requires 'pyparsing'  # not picked up in pip dependencies
+  provides []
+end
+
+dep 'numpy' do
+  requires {
+    on :ubuntu, 'numpy.managed'
+    on :osx, 'numpy.pip'
+  }
+end
+
+dep 'numpy.managed' do
+  installs 'python-numpy'
+  provides []
+end
+
+dep 'numpy.pip' do
+  provides []
+end
+
+dep 'pandas', :template => 'pip' do
+  provides []
 end
 
 dep 'pandas.managed' do
-    installs {
-        via :apt, 'python-pandas'
-    }
+  installs 'python-pandas'
+  provides []
 end
 
+dep 'pyparsing', :template => 'pip' do
+  provides []
+end
+
+dep 'python', :template => 'managed' do
+  installs {
+    via :apt, 'python-dev'
+    via :brew, 'python'
+  }
+  provides ['python']
+end
+
+dep 'python-dev.managed' do
+  provides []
+end
+
+dep 'scipy' do
+  requires {
+    on :osx, 'python', 'scipy.pip'
+    on :ubuntu, 'python', 'scipy.managed'
+  }
+end
+
+dep 'scipy.managed' do
+  installs 'python-scipy'
+  provides []
+end
+
+dep 'scipy.pip' do
+  provides []
+end
