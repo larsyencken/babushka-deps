b7df673205176383f892cedd0946c0de7f893ed7
Lars Yencken <lars@yencken.org>
2013-04-18 06:34:20 +0000

    Deps for R 3.0 on ubuntu.

--- r.rb
+++ r.rb
@@ -7,11 +7,27 @@
 R_CRAN_MIRROR="http://cran.ms.unimelb.edu.au/"
 
 dep 'R', :template => 'managed' do
+  requires {
+    on :ubuntu, 'marutter rdev ppa in sources' 
+  }
   installs {
     via :apt, 'r-base'
     via :brew, 'R'
   }
   provides ['R', 'Rscript']
+end
+
+dep 'marutter rdev ppa in sources' do
+  requires "python-software-properties.managed"
+  met? {
+    File.exist? "/etc/apt/sources.list.d/marutter-rdev-#{shell 'lsb_release -cs'}.list"
+  }
+  meet {
+    sudo "add-apt-repository -y ppa:marutter/rdev"
+  }
+  after {
+    sudo("apt-get update")
+  }
 end
 
 meta 'r' do
@@ -27,7 +43,7 @@ meta 'r' do
   }
 end
 
-dep 'r for statistics' do
+dep 'R for statistics' do
   requires 'ggplot2.r',
     'caret.r',
     'reshape.r',
@@ -44,6 +60,10 @@ dep 'medley.r' do
   meet {
       shell %[echo "library(devtools); install_github('medley', 'mewo2')" | R CMD BATCH --no-save --no-restore /dev/stdin /dev/stdout], :sudo => (! File.writable? "/usr/local/lib")
   }
+end
+
+dep 'python-software-properties.managed' do
+  provides ['add-apt-repository']
 end
 
 dep 'devtools.r'
--- servers.rb
+++ servers.rb
@@ -1,20 +1,3 @@
-meta :custom_apt_source, :for => :ubuntu do
-    accepts_value_for :key_cmd
-    accepts_value_for :source_line
-
-    template {
-        met? {
-            File.exists? "/etc/apt/sources.list.d/#{name.split('.')[0]}.list"
-        }
-
-        meet {
-            shell "#{key_cmd}"
-            shell "sudo bash -c 'echo #{source_line} >/etc/apt/sources.list.d/#{name.split('.')[0]}.list'"
-            shell "sudo apt-get update"
-        }
-    }
-end
-
 dep 'varnish', :template => 'managed' do
     requires 'varnish.custom_apt_source'
     installs {
